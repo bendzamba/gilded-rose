4f1fa937468c140472620ef5b01ab702440f8c24
bendzamba <bendzamba@gmail.com>
2024-12-05 17:22:28 -0500

    adding a nested describe block, focusing on a 'normal item' that has no special rules associated with it. checking the basic functionality and rules related to how an item degrades, the degradation limits, and the sellIn calculation.

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,14 +1,44 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should have the name foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    const firstItem = items[0];
-    expect(firstItem.name).toBe('foo');
-    expect(firstItem.quality).toBe(0);
-    expect(firstItem.sellIn).toBe(-1);
-  });
+
+  describe('Normal Item', () => {
+
+    it('should retain the name given to it', () => {
+      const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+      const items = gildedRose.updateQuality();
+      const firstItem = items[0];
+      expect(firstItem.name).toBe('foo');
+    });
+  
+    it('should decrease the sell by date by one', () => {
+      const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+      const items = gildedRose.updateQuality();
+      const firstItem = items[0];
+      expect(firstItem.sellIn).toBe(-1);
+    });
+  
+    it('should not degrade below zero', () => {
+      const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+      const items = gildedRose.updateQuality();
+      const firstItem = items[0];
+      expect(firstItem.quality).toBe(0);
+    });
+  
+    it('should degrade by one in quality', () => {
+      const gildedRose = new GildedRose([new Item('foo', 5, 10)]);
+      const items = gildedRose.updateQuality();
+      const firstItem = items[0];
+      expect(firstItem.quality).toBe(9);
+    });
+  
+    it('should degrade by two in quality once sell by date has passed', () => {
+      const gildedRose = new GildedRose([new Item('foo', 0, 20)]);
+      const items = gildedRose.updateQuality();
+      const firstItem = items[0];
+      expect(firstItem.quality).toBe(18);
+    });
+  })
 
 });
 
