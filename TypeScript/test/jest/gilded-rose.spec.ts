a37d019f5dac62f9cd69579e1ebad784e392ea91
bendzamba <bendzamba@gmail.com>
2024-12-05 17:11:21 -0500

    adding additional expectations. since there are only 3 values to check, we should be sure to check them all. quality and sellIn are actually most important to check as these are the values that are dynamic

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,14 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
+  it('should have the name foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
+    const firstItem = items[0];
+    expect(firstItem.name).toBe('foo');
+    expect(firstItem.quality).toBe(0);
+    expect(firstItem.sellIn).toBe(-1);
   });
+
 });
+
