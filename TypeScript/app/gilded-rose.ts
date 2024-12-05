bbcf14c6c717f51308dcf60ab86dbcbf992bc96e
bendzamba <bendzamba@gmail.com>
2024-12-05 18:00:10 -0500

    adding BackstagePassUpdater which handles a variety of scenarios around increasing price as sell by date nears, dropping to 0 after. Ensuring it, and our AgedBrieUpdater, do not increase value above 50

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -37,12 +37,33 @@ class AgedBrieUpdater implements ItemUpdater {
     if (item.quality < 50) {
       item.quality += item.sellIn < 0 ? 2 : 1;
     }
+    // Catch the scenario where we increased above 50
+    if (item.quality > 50) item.quality = 50;
   }
 }
 
 class SulfurasUpdater implements ItemUpdater {
   update(item: Item): void {
     // Sulfuras, being a legendary item, never changes!
+  }
+}
+
+class BackstagePassUpdater implements ItemUpdater {
+  update(item: Item): void {
+    item.sellIn -= 1;
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    } else if (item.sellIn <= 5) {
+      item.quality += 3;
+    } else if (item.sellIn <= 10) {
+      item.quality += 2;
+    } else {
+      item.quality += 1;
+    }
+    // Catch the scenario where we increased above 50
+    if (item.quality > 50) {
+      item.quality = 50;
+    }
   }
 }
 
