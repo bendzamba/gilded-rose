6de3c91c8f315dd41ae3a83ac17c46fb792c45a0
bendzamba <bendzamba@gmail.com>
2024-12-05 17:48:55 -0500

    adding ItemUpdater interface that can be implemented by various updater classes. the 'update' method should always be available, and accept an Item while returning nothing. Implement the initial 'DefaultItemUpdater' which adheres to our standard rules.

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -10,6 +10,25 @@ export class Item {
   }
 }
 
+// Define an interface for updating items
+interface ItemUpdater {
+  update(item: Item): void;
+}
+
+// Default Item Updater
+class DefaultItemUpdater implements ItemUpdater {
+  update(item: Item): void {
+    // Our sellIn day decreases by one
+    item.sellIn -= 1;
+    // If the item quality is zero, it will not decrease
+    if (item.quality > 0) {
+      item.quality -= item.sellIn < 0 ? 2 : 1;
+    }
+    // Catch the scenario where we reduced by two, from one, and now have a negative quality
+    if (item.quality < 0) item.quality = 0;
+  }
+}
+
 export class GildedRose {
   items: Array<Item>;
 
